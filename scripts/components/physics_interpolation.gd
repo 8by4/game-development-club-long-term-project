9b543fcc9b14f05341c6800d9c5bfe3885f8a0ad
TuxTheAstronaut <55007230+TuxTheAstronaut@users.noreply.github.com>
2025-12-05 18:49:47 -0500

    created player and basic floor

---
+++ scripts/components/physics_interpolation.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export_node_path("Node2D") var target_node : NodePath = NodePath("..")
+@onready var target : Node2D = get_node(target_node)
+
+var last_pos : Vector2 = Vector2()
+var current_pos : Vector2 = Vector2()
+
+func _init() -> void:
+	top_level = true
+
+func _ready() -> void:
+	global_position = target.global_position
+
+func _physics_process(_delta : float) -> void:
+	# save last position
+	last_pos = current_pos
+	# get current position
+	current_pos = target.global_position
+
+func _process(_delta : float) -> void:
+	global_position = last_pos + ((current_pos - last_pos) * Engine.get_physics_interpolation_fraction())
---
+++ scripts/entities/player/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+# movement
+var gravity : int = 512
+var walk_speed : int = 128
+var jump_height : int = 144
+# input
+var direction : float = 0
+var jump_queued : bool = false
+# nodes
+@onready var sprite : Sprite2D = $Sprite
+
+func _input(_event : InputEvent) -> void:
+	direction = Input.get_axis("move_left", "move_right")
+	if Input.is_action_just_pressed("jump") : jump_queued = true
+	
+	if direction < 0:
+		sprite.flip_h = true
+	elif direction > 0:
+		sprite.flip_h = false
+
+func _physics_process(delta : float) -> void:
+	var target_velocity : Vector2 = Vector2(direction * walk_speed, 0)
+	
+	if is_on_floor():
+		velocity = velocity.lerp(target_velocity, (16 * delta))
+		if jump_queued : velocity.y = -jump_height
+	else:
+		velocity.y += gravity * delta
+		if velocity.dot(Vector2(direction,0)) < 64:
+			velocity += target_velocity * delta
+	
+	move_and_slide()
+	jump_queued = false
