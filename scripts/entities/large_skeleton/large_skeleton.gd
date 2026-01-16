9911edc8a1e86fd8fe1eeb33e6ac7dc25096e808
Grant Ellison <gnellison2004@gmail.com>
2026-01-16 15:05:12 -0500

    add movement and player chasing for large skeleton enemy

--- scripts/entities/large_skeleton/large_skeleton.gd
+++ scripts/entities/large_skeleton/large_skeleton.gd
@@ -1,15 +1,26 @@
 extends CharacterBody2D
 
+@onready var SPEED = 50
 @onready var player : Player  = get_node("../Player")
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var chase : bool = false
 
 func _physics_process(_delta : float) -> void:
-	var direction = (player.position - self.position).normalized()
-	if direction.x > 0:
-		sprite.flip_h = false
+	if chase == true:
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
+		velocity.x = direction.x * SPEED
 	else:
-		sprite.flip_h = true
+		velocity.x = 0
+	move_and_slide()
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		pass
+		chase = true
+
+func _on_player_detection_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		chase = false
