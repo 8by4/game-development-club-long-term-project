ca28954b68a9871f8c966bc60a6ac0f3cd5a6694
TuxTheAstronaut <tuxtheastronaut@protonmail.com>
2026-01-28 16:10:55 -0500

    Update camera.gd

--- scripts/components/camera.gd
+++ scripts/components/camera.gd
@@ -2,10 +2,11 @@ extends Camera2D
 
 @export_node_path("Node2D") var target_node : NodePath = NodePath("..")
 @onready var target : Node2D = get_node(target_node)
+@export var speed : float = 5.0
 
 func _ready() -> void:
 	top_level=true
 	global_position = target.global_position
 
 func _process(delta : float) -> void:
-	global_position = lerp(global_position,target.global_position, 5 * delta)
+	global_position = lerp(global_position,target.global_position, speed * delta)
