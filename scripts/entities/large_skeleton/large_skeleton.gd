a0ecb01786bd2d3db95791478df6aa307aeda391
TuxTheAstronaut <55007230+TuxTheAstronaut@users.noreply.github.com>
2026-01-28 15:45:19 -0500

    cleaned up some of the code to fix collision issues

--- scripts/entities/large_skeleton/large_skeleton.gd
+++ scripts/entities/large_skeleton/large_skeleton.gd
@@ -1,28 +1,30 @@
 extends CharacterBody2D
 
-@onready var SPEED = 50
-@onready var player : Player  = get_node("../Player")
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
-@onready var chase : bool = false
+var chase : bool = false
+var speed : float = 50
+var target : Node2D
 
 func _physics_process(_delta : float) -> void:
 	if chase == true:
-		var direction = (player.position - self.position).normalized()
+		var direction = (target.position - self.position).normalized()
 		if direction.x > 0:
 			sprite.flip_h = false
 		else:
 			sprite.flip_h = true
-		velocity.x = direction.x * SPEED
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = 0
 	move_and_slide()
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
+		sprite.play("walk")
+		target = body
 		chase = true
-		get_node("AnimatedSprite2D").play("walk")
 
 func _on_player_detection_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
+		sprite.play("idle")
+		target = body
 		chase = false
-		get_node("AnimatedSprite2D").play("idle")
